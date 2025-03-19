c4d9e8a64a0e7fe33bbf806262ad862f68d6821b
Jack Richardson <jackrichardson376@gmail.com>
2025-03-19 17:01:56 -0600

    Create Strings.cs to hold string constants

---
+++ backend/Types/Strings.cs
@@ -0,0 +1,23 @@
+namespace backend.Types;
+
+public static class Strings {
+    public const string DISCORD = "discord";
+    public const string SESSION_ID = "session-id";
+    public const string TOKEN = "token";
+    public const string GRANT_TYPE = "grant_type";
+    public const string AUTHORIZATION_CODE = "authorization_code";
+    public const string CODE_KEY = "code";
+    public const string REDIRECT_URI = "redirect_uri";
+    public const string CALLBACK_ENDPOINT = "/api/auth/callback";
+    public const string LOGIN_ENDPOINT = "/api/login/";
+    public const string MATCHES_ENDPOINT = "/matches";
+    public const string URL_FORM_MEDIA_TYPE = "application/x-www-form-urlencoded";
+    public const string MATCHUPS_GROUP = "MATCHUPS";
+    public const string MATCHUPS_UPDATED = "MatchupsUpdated";
+    public const string MATCHUPS_COLLECTION_NAME = "matchups";
+    public const string SESSIONS_COLLECTION_NAME = "sessions";
+    public const string SETTINGS_COLLECTION_NAME = "settings";
+    public const string USERS_COLLECTION_NAME = "users";
+    public const string EXIT_MATCHUP = "ExitMatchup";
+    public const string MATCHUP_JOINED = "MatchupJoined";
+}
